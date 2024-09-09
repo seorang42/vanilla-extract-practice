23a8eb0e3c19da031fcdd9654127e311f25c047e
seorang42 <seorang42@naver.com>
2024-09-09 16:06:58 +0900

    Next.js, vanilla-extract 설치 및 초기 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>제목입니다</h1>
+      <h2>부제목입니다</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+        similique, veniam fuga illo a reiciendis, maxime necessitatibus neque
+        iure magnam vel dolorum beatae veritatis facilis incidunt nobis.
+        Voluptates, eveniet sunt?
+      </p>
+      <button>버튼입니다</button>
+    </div>
+  );
+}
